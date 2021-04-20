3353d08492d59fc6ee09153638b1c792d65098b1
supermino-ux <monofive55@gmail.com>
2021-04-20 20:56:13 +0900

    두번쨰 커밋

---
+++ project01/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request,jsonify
+from pymongo import MongoClient
+
+from scrap import my_scrap
+
+client = MongoClient('localhost', 27107)
+db = client.spartadb
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+
+    return render_template('index.html')
+
+@app.route('/news')
+def my_news():
+    my_data = my_scrap()
+    print(my_data)
+    return jsonify(my_data)
+
+if __name__ == "__main__":
+    app.run('127.0.0.1', 5000,debug=True)
---
+++ project01/project_mino.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# 아래 빈 칸('')을 채워보세요
+data = requests.get('https://search.naver.com/search.naver?&where=news&query=%EC%9B%B9%ED%88%B0', headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+# divs = soup.select('#main_pack > section.sc_new.sp_nnews._prs_nws > div > div.group_news > ul > li')
+cnt = 0
+for i in range(0,100):
+    # sp_nws1
+    content = soup.select(f'#sp_nws{i}')
+    if len(content) != 0:
+        url = content[0].select_one('div.news_wrap.api_ani_send > div > a')['href']
+        title = content[0].select_one('div.news_wrap.api_ani_send > div > a')['title']
+        description = content[0].select_one('div.news_wrap.api_ani_send > div > div.news_dsc > div > a').text
+        img = content[0].select_one('div.news_wrap.api_ani_send > a > img')['src']
+        cnt = cnt + 1
+        if cnt == 6:
+            break
+        else:
+            news  = {
+                'title':title,
+                'desc':description,
+                'img':img
+            }
+            # mongodb insert
+        print(url, title, description, img)
+
+#sp_nws1
+
+# for div in divs:
+#     print(div)
+# # 아래 빈 칸('')을 채워보세요
+# for tr in trs:
+#     rank = tr.select_one('td.number').text[0:2].strip()
+#     title = tr.select_one('a.title.ellipsis').text.strip()
+#     artist = tr.select_one('a.artist.ellipsis').text
+#     print(rank, title, artist)
+
